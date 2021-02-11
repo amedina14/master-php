2a41d8909fc73b426e147e1e867d6695e4361137
Adrian Medina <medinalarry96@gmail.com>
2021-02-11 21:53:47 +0100

    variabili

---
+++ imparando-php/03-variabili/index.php
@@ -0,0 +1,11 @@
+<?php
+
+// Variabile: contenitore d'informazione che può salvare qualsiasi dato dentro.
+$la_mia_variabile = "Ciao a tutti!";
+$numero = 77;
+$vero = true;
+
+echo '<h1>'.$la_mia_variabile.'</h1>';
+echo $numero.'<br>';
+
+?>
