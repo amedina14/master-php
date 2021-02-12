835bcbf42167f8153f2ca2bb336bf5c8e047423a
Adrian Medina <medinalarry96@gmail.com>
2021-02-12 12:15:17 +0100

    operatori aritmetici

---
+++ imparando-php/06-operatori/index.php
@@ -0,0 +1,11 @@
+<?php
+
+// Operatori Aritmetici
+$x = 65;
+$y = 33;
+
+echo 'somma: '.$x.' + '.$y.' = '.($x + $y).'<br>';
+echo 'resta: '.($x - $y).'<br>';
+echo 'moltiplicazione: '.($x * $y).'<br>';
+echo 'divisione: '.($x / $y).'<br>';
+echo 'modulo/resto: '.($x % $y).'<br>';
