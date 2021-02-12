f681d6e93ae2f2036052209c53e1f237d6567c76
Adrian Medina <medinalarry96@gmail.com>
2021-02-12 08:35:14 +0100

    sequenza di variabili

--- imparando-php/03-variabili/index.php
+++ imparando-php/03-variabili/index.php
@@ -5,7 +5,12 @@ $la_mia_variabile = "Ciao a tutti!";
 $numero = 77;
 $vero = true;
 
+$numero=1077;
+
 echo '<h1>'.$la_mia_variabile.'</h1>';
+echo $numero.'<br>';
+
+$numero=120;
 echo $numero.'<br>';
 
 ?>
