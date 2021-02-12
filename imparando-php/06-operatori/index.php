1d7686bade5f2e8f903bfaf6819e3f54e11a76f8
Adrian Medina <medinalarry96@gmail.com>
2021-02-12 12:44:07 +0100

    operatori d'incremento e decremento

--- imparando-php/06-operatori/index.php
+++ imparando-php/06-operatori/index.php
@@ -1,6 +1,7 @@
 <?php
 
 // Operatori Aritmetici
+echo "<p><b>Operatori Aritmetici</b></p>";
 $x = 65;
 $y = 33;
 
@@ -9,3 +10,14 @@ echo 'resta: '.($x - $y).'<br>';
 echo 'moltiplicazione: '.($x * $y).'<br>';
 echo 'divisione: '.($x / $y).'<br>';
 echo 'modulo/resto: '.($x % $y).'<br>';
+
+// Incremento e decremento
+echo "<p><b>Incremento e decremento</b></p>";
+$year = 2021;
+$year++;
+echo "Anno successivo: $year <br>";
+$year--;
+echo "Anno attuale: $year <br>";
+--$year;
+echo "Anno precedente: $year <br>";
+echo "<p>++\$year pre-incremento | --\$year pre-decremento</p>";
