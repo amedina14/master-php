9647f7d78f70619cad4678f9c96edaa3785c1b67
Adrian Medina <medinalarry96@gmail.com>
2021-02-12 14:26:01 +0100

    operatori d'assegnazione

--- imparando-php/06-operatori/index.php
+++ imparando-php/06-operatori/index.php
@@ -21,3 +21,17 @@ echo "Anno attuale: $year <br>";
 --$year;
 echo "Anno precedente: $year <br>";
 echo "<p>++\$year pre-incremento | --\$year pre-decremento</p>";
+
+// Operatore d'assegnazione
+echo "<p><b>Operatori d'assegnazione</b></p>";
+$eta=18;
+$eta_alterata = null;
+$eta_alterata += $eta;
+echo 'età originale: '.$eta;
+echo '<br>età più uno (18+1): '.$eta_alterata+=1; //18+1=19
+echo '<br>età originale (19-2) meno uno: '.$eta_alterata-=2; //19-2=17
+echo '<br>età originale (18*5) moltiplicata per cinque: '.$eta*=5; //18*5=90
+echo '<br>età alterata (17-90+20), più due: '.$eta_alterata-=$eta+20; //17-90=-73+20=93
+echo '<br>età originale (90/5), diviso cinque: '.$eta/=5; //90*5=18
+
+
