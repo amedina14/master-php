116bfef776822af3496b7a3a5c82f65aa9dbb4e0
Adrian Medina <medinalarry96@gmail.com>
2021-02-12 10:44:11 +0100

    Debugger, Interpolazione e caratteri speciali per console

--- imparando-php/04-tipi-di-dati/index.php
+++ imparando-php/04-tipi-di-dati/index.php
@@ -29,4 +29,29 @@ echo 'tipo di dato: '.gettype($booleano).'<br>';
 echo 'tipo di dato: '.gettype($nulla).'<br>';
 
 
+//Debugger 
+$debug= "test text debugger";
+var_dump($debug);
+
+//debug array
+echo '<br>';
+$my_array[]="valore 1";
+$my_array[]="valore 2";
+var_dump($my_array);
+
+echo '<br>';
+$second_array[5]=[1,2,'3',4,5];
+var_dump($second_array);
+
+// Interpolare dati su string
+// meno veloce perchè controlla se ci sono variabili da interpolare
+echo "<br>Interpolazione di variabili: $numero"; 
+echo '<br>Interpolazione di variabili: $numero';
+echo '<br>Interpolazione di variabili:'.$numero; // più veloce
+
+// Visualizzare su console
+echo "\n\n\tEseguire da console per vedere i risultati"
+. " di questa stringa con doppi apici/virgolette (\" \")."
+. "\r1.";
+
 ?>
