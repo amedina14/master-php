34fcb36f76af363ab1cfe039cab5e830b3287383
Adrian Medina <medinalarry96@gmail.com>
2021-02-14 20:58:08 +0100

    ciclo while, e non aggiungere la virgola all'ultimo elemento

---
+++ imparando-php/09-cicli/index.php
@@ -0,0 +1,20 @@
+<?php
+
+$num = 0;
+//$c=0;
+while($num <= 10){
+    
+    echo $num;
+    if($num != 10){
+        echo ', ';
+    }
+    /*
+    if($num != 10){
+        echo $num.', ';        
+    } else {
+        echo "$num.";
+    }
+     */
+
+    $num+=1;
+}
