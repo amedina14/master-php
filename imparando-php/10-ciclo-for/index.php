098850498a79331cff8c005480e7938c5e832c74
Adrian Medina <medinalarry96@gmail.com>
2021-02-15 12:04:45 +0100

    cycle for: validazoni BREAK se x NaN e poi se x>99 o se x<0

--- imparando-php/10-ciclo-for/index.php
+++ imparando-php/10-ciclo-for/index.php
@@ -12,17 +12,30 @@ for($i=1;$i<=100;$i++){
 }
 echo "<p>La somma del 100 numeri è: $somma</p>";
 
-// Tabellina di n
-if (isset($_GET['x'])){    
-    $x=(int)$_GET['x'];
+// Tabellina di n: (con break interrompe se compie una condizione.
+if (isset($_GET['x'])){
+    $x=$_GET['x']; //(int)
 } else {
     $x=1;
 }
 echo "<br><hr/><h3>Tabellina del $x</h3>";
 for($i=1;$i<=10;$i++){
+    
+    if(is_numeric($x)){
+        if($x>=100 or $x<=0){
+            echo "Operazione non ammessa.<br>Il numero dev'essere minore di 100"
+            . " e maggiore di 0.";
+            break;
+        }        
+    } else {
+        echo "Operazione non ammessa.<br>Il valore non è un numero";
+        break;
+    }
+    
     $result=$x*$i;
     echo "<p>$x * $i = $result</p>";
 }
+echo '<p>Arrivederci e grazie!.</p>';
 
 
 // Fibonacci di n
