59af54bcfbd198c9ff6b2ee91f88dd9779078cf5
Adrian Medina <medinalarry96@gmail.com>
2021-02-15 12:24:00 +0100

    Esercizi: esercizio 1, stampa strings e ottieni e stampa tipo

---
+++ imparando-php/11-esercizi/esercizio1_PaeseContinente.php
@@ -0,0 +1,13 @@
+<?php
+
+/* 
+ Creare 2 variabili "paese" e "continente" e mostrare il suo valore su schermo.
+ Collocare commento con il suo tipo di dato.
+ */
+
+$paese="Italia";
+$continente="Europa";
+
+echo "<p><b>Paese:</b> $paese - <b>Tipo dato:</b> ".gettype($paese)."</p>"; // string
+echo "<p><b>Continente:</b> $continente - <b>Tipo dato:</b> ".gettype($continente)."</p>"; // string
+
