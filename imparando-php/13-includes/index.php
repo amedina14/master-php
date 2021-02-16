b961842d42712466a146690e2a66bc2f5fa58b51
Adrian Medina <medinalarry96@gmail.com>
2021-02-16 13:28:24 +0100

    Require e require_once

--- imparando-php/13-includes/index.php
+++ imparando-php/13-includes/index.php
@@ -1,4 +1,11 @@
-<?php include './includes/header.php'; ?>
+<!-- 
+    Require: Non carica la pagina se non riceve bene il file.
+    Include: Carica la pagina con errori anche se non ha ricevuto 
+    bene il file/importazione.
+
+    require_once: Non permette di ripetere l'importazione/elemento.
+-->
+<?php require_once './includes/header.php'; ?>
 
 <!--Contenuto-->
 <div>
@@ -9,4 +16,4 @@
 var_dump($nome);
 ?>
 
-<?php include './includes/footer.php'; ?>
+<?php include_once './includes/footer.php'; ?>
